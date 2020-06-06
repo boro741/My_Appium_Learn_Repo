62a79dcc849b6ce853ba878e009d8b651981a4f1
Pavan Boro <Pavan.Boro@resideo.com>
2020-06-06 16:42:06 +0530

    ios setup done

--- src/appium_learn/BaseDriver.java
+++ src/appium_learn/BaseDriver.java
@@ -8,20 +8,23 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class BaseDriver {
-	public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
-		File f = new File("src");
-		File fs = new File(f,"ApiDemos-debug.apk");
+	public static IOSDriver<IOSElement> Capabilities() throws MalformedURLException {
+		File f = new File("//Users/qatest//Library//Developer//Xcode//DerivedData//UICatalog-dwlkcbkzqblmkadwkjbwtnaryqsg//Build//Products//Debug-iphonesimulator");
+		File fs = new File(f,"UICatalog.app");
 		
 		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "testPixel");
-		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "IOS");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,AutomationName.IOS_XCUI_TEST);
 		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "testPixel");
 		
-		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+		IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
 		
 		return driver;
 	}
--- src/appium_learn/Suite_Runner.java
+++ src/appium_learn/Suite_Runner.java
@@ -4,12 +4,14 @@ import java.net.MalformedURLException;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
 
 public class Suite_Runner extends BaseDriver {
 
 	public static void main(String[] args) throws MalformedURLException {
 		
-		AndroidDriver<AndroidElement> driver = Capabilities();
+		IOSDriver<IOSElement> driver = Capabilities();
 	}
 
 }
